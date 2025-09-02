e2b04de30c8f85dfbde1d420acdd04d22a7d0586
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-09-02 21:57:24 +0200

    Refactored Button Component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,10 +2,13 @@ import type { ComponentProps, PropsWithChildren } from "react";
 import styles from "./Button.module.css";
 
 // https://www.totaltypescript.com/react-component-props-type-helper
-const Button = (props: PropsWithChildren<ComponentProps<"button">>) => {
+const Button = ({
+  children,
+  ...props
+}: PropsWithChildren<ComponentProps<"button">>) => {
   return (
     <button className={styles.button} {...props}>
-      {props.children}
+      {children}
     </button>
   );
 };
