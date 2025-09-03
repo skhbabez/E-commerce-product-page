974f1318e252fe530047d80e370a6f929e81068a
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-09-03 23:30:49 +0200

    basic popover implemented

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,29 +1,38 @@
-import { useContext } from "react";
+import { useContext, useId } from "react";
 import Button from "../button";
 import styles from "./Cart.module.css";
 import { CartCtx } from "../../context/cartContext";
 import CartItem from "../CartItem";
+import ShoppingCart from "../icons/ShoppingCart";
 
 const Cart = () => {
   const { cart, removeItem } = useContext(CartCtx);
+  const id = useId();
   return (
-    <div className={styles.cart}>
-      <h2>Cart</h2>
-      {cart.length > 0 ? (
-        <>
-          <ul>
-            {cart.map((item) => (
-              <li key={item.id}>
-                <CartItem {...item} onDelete={removeItem} />
-              </li>
-            ))}
-          </ul>
-          <Button>Checkout</Button>
-        </>
-      ) : (
-        <p className={styles.emptymessage}>Your cart is empty.</p>
-      )}
-    </div>
+    <>
+      <button className={styles.shopbutton}  popoverTarget={id}>
+        <ShoppingCart />
+      </button>
+      <div className={styles.popover} popover="auto" id={id}>
+        <div className={styles.cart}>
+          <h2>Cart</h2>
+          {cart.length > 0 ? (
+            <>
+              <ul>
+                {cart.map((item) => (
+                  <li key={item.id}>
+                    <CartItem {...item} onDelete={removeItem} />
+                  </li>
+                ))}
+              </ul>
+              <Button>Checkout</Button>
+            </>
+          ) : (
+            <p className={styles.emptymessage}>Your cart is empty.</p>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 export default Cart;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import Avatar from "../avatar";
 import ImageLink from "../imageLink";
 import { UserCtx } from "../../context/userContext";
+import Cart from "../cart";
 
 // Use A router for next project
 const Header = () => {
@@ -11,6 +12,7 @@ const Header = () => {
       <ImageLink href="#">
         <Avatar alt="User Avatar" src={user.avatar}></Avatar>
       </ImageLink>
+      <Cart></Cart>
     </header>
   );
 };
