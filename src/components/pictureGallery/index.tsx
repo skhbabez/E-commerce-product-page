d974d5cf998f3ec7e667c8da24b0b8793ba1881e
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-09-06 17:13:29 +0200

    added lightbox gallery

--- src/components/pictureGallery/index.tsx
+++ src/components/pictureGallery/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./PictureGallery.module.css";
 import type { Image } from "../../types";
-import { useState } from "react";
-import GalleryNavigation from "./galleryNavigation";
+import { useRef, useState } from "react";
+import ThumbnailNavigation from "./thumbnailNavigation";
+import Close from "../icons/Close";
+import LeftArrow from "../icons/LeftArrow";
+import RightArrow from "../icons/ArrowRight";
 
 interface PictureGalleryProps {
   images: Image[];
@@ -9,41 +12,102 @@ interface PictureGalleryProps {
 
 const PictureGallery = ({ images }: PictureGalleryProps) => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
   if (images.length <= 0) {
     return <div>no images</div>;
   }
+  const showLightbox = () => {
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  };
+  const closeLightbox = () => {
+    if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+  };
+
+  const onPrevClick = () => {
+    currentIndex > 0 ? currentIndex - 1 : images.length - 1;
+  };
+  const onNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
   const currentImage = images[currentIndex];
   return (
-    <div className={styles.gallery}>
-      <div className={styles.currentimage}>
-        <GalleryNavigation
-          className={styles.buttonnavigation}
-          onNextClick={() =>
-            setCurrentIndex((current) => (current + 1) % images.length)
-          }
-          onPrevClick={() =>
-            setCurrentIndex((current) =>
-              current > 0 ? current - 1 : images.length - 1
-            )
-          }
+    <>
+      <div className={styles.embeddedgallery}>
+        <div className={styles.currentimage}>
+          <div className={styles.navigation}>
+            <button
+              type="button"
+              aria-label="previous image"
+              onClick={onPrevClick}
+            >
+              <LeftArrow />
+            </button>
+            <button type="button" aria-label="next image" onClick={onNextClick}>
+              <RightArrow />
+            </button>
+          </div>
+
+          {/* how to make this meaningufully accessible? */}
+          <button
+            type="button"
+            onClick={showLightbox}
+            aria-label="open lightbox gallery"
+            aria-haspopup="dialog"
+          >
+            <img src={currentImage.image} alt={currentImage.alt} />
+          </button>
+        </div>
+        <ThumbnailNavigation
+          images={images}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+          name="embedded"
         />
-        <img src={currentImage.image} alt={currentImage.alt}></img>
       </div>
-      <fieldset className={styles.thumbnailnavigation}>
-        <legend className="sr-only">Productimages</legend>
-        {images.map((image, index) => (
-          <label key={image.id}>
-            <input
-              type="radio"
-              name="productimage"
-              checked={image.id === currentImage.id}
-              onChange={() => setCurrentIndex(index)}
-            />
-            <img src={image.thumbnail} alt={image.alt}></img>
-          </label>
-        ))}
-      </fieldset>
-    </div>
+
+      <dialog className={styles.lightboxdialog} ref={dialogRef}>
+        <div className={styles.lightboxgallery}>
+          <button
+            className={styles.closebutton}
+            aria-label="close lightbox"
+            onClick={closeLightbox}
+          >
+            <Close />
+          </button>
+          <div className={styles.currentimage}>
+            <div className={styles.navigation}>
+              <button
+                type="button"
+                aria-label="previous image"
+                onClick={onPrevClick}
+              >
+                <LeftArrow />
+              </button>
+              <button
+                type="button"
+                aria-label="next image"
+                onClick={onNextClick}
+              >
+                <RightArrow />
+              </button>
+            </div>
+            <img src={currentImage.image} alt={currentImage.alt}></img>
+          </div>
+
+          <ThumbnailNavigation
+            className={styles.thumbnailwrapper}
+            images={images}
+            currentIndex={currentIndex}
+            setCurrentIndex={setCurrentIndex}
+            name="lightbox"
+          />
+        </div>
+      </dialog>
+    </>
   );
 };
 
